2394876f17b764573adc1a8756f316e99357aa3c
Davide Stenner <48441753+DavideStenner@users.noreply.github.com>
2025-01-17 15:32:39 +0100

    argument fix

--- scrape.py
+++ scrape.py
@@ -5,7 +5,7 @@ if __name__=='__main__':
     from src.scraper.fanta.scraper import ScraperFanta
     
     parser = argparse.ArgumentParser()
-    parser.add_argument('--league', type=str, default="TicketOne")
+    parser.add_argument('--league', type=str, default="Motorola")
     parser.add_argument('--number_page_scrape', type=int, default=None)
     parser.add_argument('--pct_scrape', type=float, default=0.3)
     parser.add_argument('--backup', type=int, default=1000)
@@ -13,6 +13,7 @@ if __name__=='__main__':
     parser.add_argument('--check_unique_name', action='store_true')
     parser.add_argument('--keep_no_pic', action='store_true')
     parser.add_argument('--scrape_all_league', action='store_true')
+    parser.add_argument('--test', action='store_true')
     args = parser.parse_args()
 
     if args.scrape_all_league:
@@ -26,18 +27,17 @@ if __name__=='__main__':
                 selected_league=league_name,
                 check_unique_name=args.check_unique_name, keep_no_pic=args.keep_no_pic,
                 number_page_scrape=args.number_page_scrape, pct_scrape=args.pct_scrape, 
-                backup = args.backup, keep_active_pc_iteration = args.keep_active_pc_iteration
+                backup = args.backup, keep_active_pc_iteration = args.keep_active_pc_iteration,
+                test=args.test
             )
-            scraper.activate_bot()
-            scraper.quit()
+            scraper()
     else:
         print(f'\n\nStarting {args.league}')
         scraper = ScraperFanta(
             selected_league=args.league,
             check_unique_name=args.check_unique_name,
             number_page_scrape=args.number_page_scrape, pct_scrape=args.pct_scrape, 
-            backup = args.backup, keep_active_pc_iteration = args.keep_active_pc_iteration
+            backup = args.backup, keep_active_pc_iteration = args.keep_active_pc_iteration,
+            test=args.test
         )
-        scraper.activate_bot()
-        scraper.quit()
-
+        scraper()
